80ec818ab974a6522d04041cf6dc8b7bc00a6d79
OrenVaknin <136793129+OrenVaknin@users.noreply.github.com>
2024-11-20 13:39:50 +0200

    Time: 1 ms (86.66%) | Memory: 40.7 MB (75.08%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        boolean negative = false;
+        if (x<0){
+            negative = true;
+            x=x*-1;
+        }
+        long num=0;
+        if(Math.log(x)/Math.log(2)>=32)
+            return 0;
+        while (x>0){
+            num=x%10+num*10;
+            x=x/10;
+        }
+        if (num>Integer.MAX_VALUE)
+            return 0;
+        if (negative)
+            num=num*-1;
+        return (int) num;
+    }
+}
