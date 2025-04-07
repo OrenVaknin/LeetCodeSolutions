579cee291b2d457f91fe50127644f29f755b4587
OrenVaknin <136793129+OrenVaknin@users.noreply.github.com>
2025-04-07 19:07:57 +0300

    Time: 2 ms (74.95%) | Memory: 53.4 MB (29.30%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -8,15 +8,5 @@ class Solution {
             cntr+=(num == unq)? 1:-1;
         } 
         return unq;
-
     } 
 }
-
-     //   int counter;
-   //     for(int j = 0; j<nums.length; j++){
- //           counter=0;
-           // for(int i=0; i<nums.length; i++){
-         //       if(nums[i]==nums[j]) counter++;
-       //     } if(counter>nums.length/2) return nums[j];
-     //       j++;}
-   //     return nums[0];
